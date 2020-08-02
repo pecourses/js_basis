b08d8291bf12d6df51b0db9f828df14ed733c379
Vladimir <freshitpte@gmail.com>
2020-08-02 17:22:13 +0300

    type conversion example

---
+++ assets/js/index.js
@@ -0,0 +1,61 @@
+"use strict";
+
+// Именование переменных A-Za-z, 0-9, _, $
+// Нельзя использовать числа в начале и ключевые слова
+
+// Однострочный коментарий
+
+/*
+
+Многострочные
+коментарии
+
+*/
+/* 
+let name = "Vasya";
+let string = `Your name is ${name}`;
+console.log(string);
+
+let check = false; // boolean
+
+let undefinedVariable; // undefined
+
+console.log(typeof undefinedVariable);
+
+let nullValule = null; 
+
+console.log(typeof null);
+
+
+let nameTest;
+
+nameTest = 20;
+ */
+///============
+
+/* let inputOne = prompt("enter your num");
+let inputTwo = prompt("enter your num2");
+
+inputOne = +inputOne;
+inputTwo = +inputTwo;
+
+ 
+let numberOne = Number(inputOne);
+let numberTwo = Number(inputTwo); 
+let result = numberOne + numberTwo;
+
+console.log(result);
+console.log( isNaN(result) ); */
+/* 
+let testValue = String(50);
+console.log(testValue + " strings");
+
+let num = 10;
+let boolValue = Boolean(num);
+
+console.log(num);
+console.log(boolValue);
+
+ */
+
+
