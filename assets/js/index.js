7a00a803626d7e8446c851ac07c4af727ce51778
Vladimir <freshitpte@gmail.com>
2020-08-08 20:24:37 +0300

    Car constructor example

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,88 +1,99 @@
 "use strict";
+/* 
+const key = "color";
 
-let test = "test";
-
-const result = isAdult(5);
-
-//console.log(cube(3)); error!
-
-const cube = function (num) {
-  // function expression
-  return num ** 3;
+const sayHi = function () {
+  return "wrong function";
 };
 
-console.log(cube(3)); //ok
+const cat = {
+  color: "red",
+  weight: 3500,
+  amountOfLegs: 4,
+  breed: "sphinx",
+  isMale: true,
+  head: {
+    amountOfEyes: 2,
+    eyeColor: "grey",
+  },
 
-/**
- *
- * @param {number} a - number to square
- * @returns {number}
- */
-
-function square(a) {
-  console.log(a);
-
-  let number = Number(a);
-
-  return number * number;
-}
-
-function isAdult(age) {
-  // function declaration
-
-  console.log(test);
-
-  let number = Number(age);
-
-  let result = isNaN(number);
-
-  if (result) {
-    return false;
-  }
-
-  if (number >= 18) {
-    return true;
-  }
-  return false;
-}
-
-// let input = prompt("Enter your number");
-// let input2 = prompt("Enter your number");
-
-// const res2 = power(+input, +input2);
-// console.log(res2);
-
-function power(base, exp) {
-  let result = base;
-
-  for (let i = 1; i < exp; i++) {
-    result *= base;
-  }
-
-  return result;
-}
-
-/**
- *
- * @param {number} a
- * @param {number} b
- * @returns {number}
- */
-
-function getMaxValue(a, b) {
-  if (a > b) {
-    return a;
-  }
-  return b;
-}
-
-const isEven = function (number) {
-  return number % 2 === 0;
+  sayHi() {
+    return "Meow!";
+  },
 };
 
-const testFunc = isEven;
+console.log(cat);
+console.log(cat.head.eyeColor);
 
-console.log(testFunc);
+const cat2 = {
+  color: "white",
+  weight: 5500,
+  amountOfLegs: 4,
+  breed: "sphinx",
+  isMale: false,
+  head: {
+    amountOfEyes: 2,
+    eyeColor: "grey",
+  },
 
+  sayHi() {
+    return "Meow!";
+  },
+}; */
+/* 
+function Cat(color, weight, breed, name) {
+  this.name = name;
+  this.color = color;
+  this.breed = breed;
+  this.weight = weight;
 
+  this.present = function () {
+    return `My name is ${this.name}`;
+  };
 
+  this.sayHi = function () {
+    return "meow";
+  };
+}
+
+const cat1 = new Cat("white", 4000, "sphinx", "Vasya");
+const cat2 = new Cat("white", 3500, "sphinx", "Pete");
+const cat3 = new Cat("blue", 4200, "siams", "Vlad");
+const cat4 = new Cat("red", 2000, "sphinx", "Murzik");
+
+cat4.color = "black";
+
+console.log(cat1, cat2, cat3, cat4);
+ */
+
+function Country(name, population, area) {
+  this.name = name;
+  this.population = population;
+  this.area = area;
+  // this.density = this.population / this.area; logic ERROR
+  this.getDensity = function () {
+    return this.population / this.area;
+  };
+}
+
+const country1 = new Country("USA", 350000000, 120000);
+
+console.log(country1);
+
+function Car(model) {
+  this.model = model;
+  this.speed = 0;
+  this.isGoing = false;
+
+  this.checkSpeed = function () {
+    this.isGoing = this.speed !== 0;
+  };
+
+  this.accelerate = function (speed) {
+    this.speed += speed;
+    this.checkSpeed();
+    return this.speed;
+  };
+}
+
+const car1 = new Car("Tesla");
